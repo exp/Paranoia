ffa50b62b44ad2a9ba0736ceb48615874a9f4cd1
Paul Robins <paul@wza.us>
2012-10-06 21:20:01 +0100

    Added random lines to the plotter, does nothing particularly special and isn't likely to be as useful as hoped

--- data/generate-office.pl
+++ data/generate-office.pl
@@ -4,6 +4,9 @@ use Modern::Perl    '2012';
 use POSIX           qw/ceil/;
 use Image::Magick;
 use Math::Vector::Real;
+use Data::Dump      'pp';
+
+use constant    ATOM    => 2;
 
 sub rn {
 	my $start   = shift;
@@ -47,10 +50,8 @@ sub render {
 		my $v1      = ${$wall}[0];
 		my $v2      = ${$wall}[1];
 		my $res     = $v1 + $v2;
-		say "Vector is $v1 -> $res";
 
 		my $points  = vec_format($v1,$scale) . " " . vec_format($res,$scale);
-		say "Points are $points";
 
 		# Draw the shit
 		$image->Draw(
@@ -62,10 +63,8 @@ sub render {
 
 	my $scaley  = $y / $scale + 35;
 	my $scalex  = 1 / $scale + 25;
-	say "Scalex: $scalex, scaley: $scaley";
 	my $points  = "25,$scaley $scalex,$scaley";
 
-	say "Points: $points";
 	# Add scale
 	$image->Draw(
 		primitive   => 'line',
@@ -104,13 +103,100 @@ sub render {
 	say "Wrote $filename$id.png";
 }
 
+sub clear_space {
+	my $min     = shift;
+	my $max     = shift;
+	my $atom    = shift;    # The minimum space on each side of a line
+	my $points  = shift;
+
+	my @checks  = sort {$a <=> $b} (@$points, $max);
+
+	my $last    = 0;
+	my $space   = 0;
+	for my $point (@checks) {
+		# If there's enough space between this point and the last, add
+		# the space
+		my $avail   = $point - $last - $atom*2;
+		if ($avail >= 0) {
+			$space  += $avail;
+		}
+		$last   = $point;
+	}
+
+	return $space;
+}
+
+sub extrap_space {
+	my $min     = shift;
+	my $max     = shift;
+	my $atom    = shift;
+	my $pos     = shift;
+	my $points  = shift;
+
+	my @sorted  = sort {$a <=> $b} @$points;
+
+	# Trim the side
+	$min    += $atom;
+
+	# Add the far wall to the points
+	push(@sorted,$max);
+
+	# If we have no points, we insert the point at its original position
+	unless (@$points) {
+		return $min + $pos;
+	}
+
+	for my $point (@sorted) {
+		# If there is at least 2 * atom free space then the point goes
+		# there, otherwise add the distance and move to the next line
+		# Min represents the leftmost point that could be placed
+		my $space   = $point - $min - $atom;
+		if ($pos >= 0 && ($space - $pos) >= 0) {
+			return $min + $pos;
+		} elsif ($pos >= 0 && $space > 0) {
+			$min    = $point + $atom;
+			$pos    -= $space;
+		} elsif ($pos > 0) {
+			$min    = $point + $atom;
+		} else {
+			return undef;
+		}
+	}
+
+	return undef;
+}
+
+sub add_divisions {
+	my $x       = shift;
+	my $office  = shift;
+	my @points;
+
+	# While there are still gaps where lines could be added
+	# grab the available free space, pick a point within it and
+	# then extrapolate its final position
+	my $count   = 0;
+	my $atom    = 1;
+	while ((my $size = clear_space(0,$x,ATOM,\@points)) >= ATOM) {
+		my $position    = sprintf("%.2f",rn(0,$size));
+		my $finalpos    = extrap_space(0,$x,ATOM,$position,\@points);
+
+		# If there's nowhere to fit this entry we have filled all
+		# available space and should return
+		last unless ($finalpos);
+
+		push (@points, $finalpos);
+	}
+
+	return @points;
+}
+
 # Offices are represented as a series of vectors in an array
 my @office;
 
-# Set the office size, anywhere from 1:1 to 4:1 from 10 to 50 metres 
+# Set the office size, anywhere from 1:1 to 4:1 from 10 to 30 metres 
 # on the short side
 my $ratio   = sprintf("%.2f",rn(1,4));
-my $y       = sprintf("%.2f",rn(10,50));
+my $y       = sprintf("%.2f",rn(10,30));
 my $x       = sprintf("%.2f",$y*$ratio);
 
 say "Office size is ${x}m x ${y}m";
@@ -119,5 +205,16 @@ push(@office,[V(0 , 0),V($x, 0)]);
 push(@office,[V(0 , 0),V(0 ,$y)]);
 push(@office,[V(0 ,$y),V($x, 0)]);
 push(@office,[V($x, 0),V(0 ,$y)]);
+
+# Add random partition lines
+my @horiz   = add_divisions($x,$y,\@office);
+my @vert    = add_divisions($y,$x,\@office);
+
+for my $line (@horiz) {
+	push @office, [V($line,0),V(0,$y)];
+}
+for my $line (@vert) {
+	push @office, [V(0,$line),V($x,0)];
+}
 
 render($x,$y,\@office)
